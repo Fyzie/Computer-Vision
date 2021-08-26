2d9b5d3c0f0275e884e0e31b26691c26c8fa20fb
Hafizi <76240694+Fyzie@users.noreply.github.com>
2021-08-26 22:19:53 +0800

    Update 02-face_detection.py

--- 02-face_detection.py
+++ 02-face_detection.py
@@ -14,7 +14,8 @@ faces = face_cascade.detectMultiScale(gray, 1.3, 5) #identify face boundaries/ f
 # scaleFactor --> image detection scaling
 # depend on the wanted objects, the higher value may occur higher risk of missing faces, the lesser value may occur wrong/ invalid object(face) detection
 # the larger the object, the higher the value needed and vice versa
-# minNeihbors --> preferable 3~6, the higher value the fewer the detections the higher quality(less wrong detections)
+# the lower the value, the higher the risk you may get false-positives on the same image scale
+# minNeighbors --> preferable 3~6, the higher value the fewer the detections the higher quality(less wrong detections)
 print(faces) #print face boundaries
 
 for (x,y,w,h) in faces: 
