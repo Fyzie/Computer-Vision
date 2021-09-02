571781d863eeaef8fb9b5a90be443385ecf1d6b0
Hafizi Malik <76240694+Fyzie@users.noreply.github.com>
2021-09-02 21:10:04 +0800

    Create 08-edge_detection_live_cam.py

---
+++ 08-edge_detection_live_cam.py
@@ -0,0 +1,15 @@
+import cv2
+
+vid = cv2.VideoCapture(0)
+
+while True:
+    _, frame = vid.read() #read vid frame by frame
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) #convert to grayscale
+    edges = cv2.Canny(gray, 30, 100)
+    cv2.imshow("edges", edges)
+    cv2.imshow("gray", gray)
+    if cv2.waitKey(1) == ord("q"): #press q to end program
+        break
+
+vid.release()
+cv2.destroyAllWindows()
